bea3fc7de4594a2d4ce9e711f6303a93e229a8f3
felipecardosodeoliveira <felipecardosodeoliveira02@gmail.com>
2025-12-05 00:05:47 -0300

    feat: adicionando maven

---
+++ src/test/java/com/fcolabs/dscommerce/DscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcolabs.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
