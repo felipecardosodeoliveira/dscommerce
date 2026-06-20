0367cfc3145320b0923c9f967228255a73cfd25d
felipecardosodeoliveira <felipecardosodeoliveira02@gmail.com>
2026-06-20 17:13:12 -0300

    feat: busca usuário

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -55,11 +55,15 @@ INSERT INTO tb_product_category (product_id, category_id) VALUES (23, 3);
 INSERT INTO tb_product_category (product_id, category_id) VALUES (24, 3);
 INSERT INTO tb_product_category (product_id, category_id) VALUES (25, 3);
 
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$Aq7hJgaFt9z550hKsLWFHeEot6O0hcRJFeZRS.Iw6v6liPBLWooju');
+/**INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$Aq7hJgaFt9z550hKsLWFHeEot6O0hcRJFeZRS.Iw6v6liPBLWooju');
 INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$Aq7hJgaFt9z550hKsLWFHeEot6O0hcRJFeZRS.Iw6v6liPBLWooju');
+**/
+
+INSERT INTO tb_user (name, email, phone, password, birth_date) VALUES ('Maria Brown', 'maria@gmail.com', '988888888', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG', '2001-07-25');
+INSERT INTO tb_user (name, email, phone, password, birth_date) VALUES ('Alex Green', 'alex@gmail.com', '977777777', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG', '1987-12-13');
 
 
-INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_CLIENT');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
 
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
